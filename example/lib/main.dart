08dc73e85f7f0d7486c306abe90bfd42e02889ae
wildflu <walidcode0@gmail.com>
2023-09-18 11:18:10 +0100

    valid

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,24 +17,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: SafeArea(
-      //   child: Center(
-          // child: AwsCard(
-          //   description: 'None For Now',
-          //   oldPrice: 35,
-          //   price: 30.24,
-          //   promution: 12,
-          //   image: Image.network( 'https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg?auto=compress&cs=tinysrgb&w=600', fit: BoxFit.cover,height: double.infinity,),
-          //   size: 100,
-          //   text: 'some tolk about this product',
-          //   tails: const ['X','S'],
-          //   prodColorsDespo: [Colors.red, Colors.black, Colors.orange, Colors.grey],
-          //   onBuyClicked: (){
-          //     print('hi');
-          //   },
-          // ),
-        // ),
-      // )
       body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 0.5),
         itemCount: 5,
